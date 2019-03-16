08d99887afca19b482b51915b4084757c69f76a5
Abu Al Mahadi <mohammed.abumahadi@northsouth.edu>
2019-03-16 23:37:33 +0600

    feature tests

--- Project 1/Documentation/Code/assist_bot/tests/Feature/ExampleTest.php
+++ Project 1/Documentation/Code/assist_bot/tests/Feature/ExampleTest.php
@@ -15,7 +15,15 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
-
+        $response = $this->get('/dashboard');
+        $response = $this->get('/messages');
+        $response = $this->get('/create');
+        $response = $this->get('/search');
+        $response = $this->get('/download');
+        $response = $this->get('/createNewProfile');
+        $response = $this->get('/botman');
+        $response = $this->get('/login/google');
+        $response = $this->get('/login/google/callback');
         $response->assertStatus(200);
     }
 }
