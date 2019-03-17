a8ba0bcf178cfddab1e484c1018ee9a2341d1aab
Abu Al Mahadi <mohammed.abumahadi@northsouth.edu>
2019-03-17 12:21:28 +0600

    New Functions Added in Feature

--- Project 1/Documentation/Code/assist_bot/tests/Feature/ExampleTest.php
+++ Project 1/Documentation/Code/assist_bot/tests/Feature/ExampleTest.php
@@ -15,15 +15,78 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function testDashBoard()
+    {
         $response = $this->get('/dashboard');
+        $response->assertStatus(200);
+    }
+
+    public function testMessages()
+    {
         $response = $this->get('/messages');
-        $response = $this->get('/create');
-        $response = $this->get('/search');
-        $response = $this->get('/download');
+        $response->assertStatus(200);
+    }
+
+    public function testFormCreate()
+    {
+        $response = $this->get('forms/create');
+        $response->assertStatus(200);
+    }
+
+    public function testFormSearch()
+    {
+        $response = $this->get('forms/search');
+        $response->assertStatus(200);
+    }
+
+    public function testFormDownload()
+    {
+        $response = $this->get('forms/download');
+        $response->assertStatus(200);
+    }
+
+    public function testQACreate()
+    {
+        $response = $this->get('qa/create');
+        $response->assertStatus(200);
+    }
+
+    public function testTagCreate()
+    {
+        $response = $this->get('tag/create');
+        $response->assertStatus(200);
+    }
+
+    public function testTagIndex()
+    {
+        $response = $this->get('tag/index');
+        $response->assertStatus(200);
+    }
+
+    public function testCreateNewProfile()
+    {
         $response = $this->get('/createNewProfile');
+        $response->assertStatus(200);
+    }
+
+    public function testBotman()
+    {
         $response = $this->get('/botman');
-        $response = $this->get('/login/google');
-        $response = $this->get('/login/google/callback');
+        $response->assertStatus(200);
+    }
+
+    public function testGoogleLogin()
+    {
+        $response = $this->get('login/google');
+        $response->assertStatus(200);
+    }
+
+    public function testGoogleCallback()
+    {
+        $response = $this->get('login/google/callback');
         $response->assertStatus(200);
     }
 }
