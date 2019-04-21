d38ed7828639e80f349734c572a7ff5d6d71ac34
Mohammed Abu Al Mahadi <mohammed.abumahadi@northsouth.edu>
2019-04-21 12:04:51 +0600

    StoreFileTest

---
+++ Project 1/Code/assist_bot/tests/Unit/store_file_required_test.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+class store_file_required_test extends TestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->user = User::first();
+    }
+
+
+    /** 
+     * 3 assertions but show 1, both are valid.
+     * @test
+     */
+    public function store_file_required()
+    {
+        $response = $this->actingAs($this->user)->post('/forms/create', [
+            'title'=>'hello',
+            'description'=>'description',
+            'file'=> '',
+            'tags'=>[1, 2]
+        ]);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors([
+            'file' => 'The file field is required.'
+        ]);
+    }
+
+
+}
